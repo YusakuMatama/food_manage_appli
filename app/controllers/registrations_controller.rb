374a471b906d9afbdf5c1f906faa26281e9a0081
YusakuMatama <kotatsu0715@gmail.com>
2019-07-26 15:21:14 +0900

    test

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,9 +1,9 @@
 class RegistrationsController < Devise::RegistrationsController
-  def after_sign_up_path_for(resource)
-    new_user_path
-  end
+  # def after_sign_up_path_for(resource)
+  #   new_user_path
+  # end
 
-  def after_inactive_sign_up_path_for(resource)
-    new_user_path
-  end
+  # def after_inactive_sign_up_path_for(resource)
+  #   new_user_path
+  # end
 end
